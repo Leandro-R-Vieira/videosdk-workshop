0ee6dbc0b78d0cc4628da1fe1a419a509cc8f72f
Leandro Rezende Vieira <103189479+Leandro-R-Vieira@users.noreply.github.com>
2024-01-07 20:28:04 -0300

    Developer Workshop 2

--- src/Session/Video.jsx
+++ src/Session/Video.jsx
@@ -1,4 +1,23 @@
-const Video = () => {
-  return <h1>Video goes here</h1>;
+import { useContext, useEffect } from "react";
+import { ClientContext, UserContext } from "../Context/globalContext";
+
+
+const Video = ({ handleClose, handleJoin}) => {
+  const uitoolkit = useContext(ClientContext);
+  const meetingConfig = useContext(UserContext);
+
+  useEffect(() => {
+    let sessionContainer = document.getElementById("session")
+    uitoolkit.joinSession(sessionContainer, meetingConfig)
+    uitoolkit.onSessionClosed(handleClose)
+    uitoolkit.onSessionJoined(handleJoin)
+
+    return () => {
+      uitoolkit.offSessionClosed(handleClose)
+      uitoolkit.offSessionJoined(handleJoin)  
+      uitoolkit.closeSession(sessionContainer)}
+  }, [uitoolkit, meetingConfig, handleClose, handleJoin]);
+
+  return <div id="session"></div>;
 };
 export default Video;
--- src/Session/index.jsx
+++ src/Session/index.jsx
@@ -1,7 +1,32 @@
-import "./Session.css";
+import { useContext, useEffect, useState } from "react"
+import { useNavigate } from "react-router-dom"
+import { UserContext } from "../Context/globalContext.js"
+
+import Preview from "./Preview.jsx"
+import Video from "./Video.jsx"
+import "./Session.css"
 
 const Session = () => {
-  return <h1>Session</h1>;
+  const [state, setState] = useState("preview")
+  const meetingArgs = useContext(UserContext)
+  const navigate = useNavigate()
+
+  const handleJoin = () => {
+    console.log("Sessão iniciada")    
+  }
+  const handleClose = () => {
+    console.log("Sessão Encerrada")
+    navigate("/")
+  }
+
+  useEffect(() => {
+    if (meetingArgs.videoSDKJWT === "") navigate("/")
+  }, [meetingArgs.videoSDKJWT, navigate])
+
+  if (state === "preview") 
+    return <Preview handleOk={() => setState("video")}/>
+  if (state === "video") 
+    return <Video handleJoin={handleJoin} handleClose={handleClose}/>  
 };
 
 export default Session;
